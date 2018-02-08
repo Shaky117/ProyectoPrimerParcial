e5d527b82a73cf5d787d578c6f61f6eaddf02839
Maestro <Maestro@figfel.local>
2018-02-07 17:43:42 -0700

    Primera version

--- ViewsSeparados/ViewController.swift
+++ ViewsSeparados/ViewController.swift
@@ -10,16 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var vwLibro: PaginaUno!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        UIView.transition(with: vwLibro, duration: 2.0, options: [.transitionCurlDown], animations: {
+           
+            
+            for vista in self.vwLibro.subviews{
+                vista.removeFromSuperview()
+            }
+                self.vwLibro.addSubview(PaginaUno(frame: self.vwLibro.bounds))
+        }, completion: nil)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
